16d954652e20fba475a85d64bafd24089b12575b
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-03-25 15:22:37 +0300

    Update
    Canvas save and shape fixes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 
 context.strokeStyle = '#000000';  // Set the color of the lines to black
-context.lineWidth = 5;  // Set the width of the lines to 5 pixels
+context.lineWidth = 4;  // Set the width of the lines to 5 pixels
 context.lineJoin = 'round';  // Makes the line joins round
 context.lineCap = 'round';  // Makes the line caps round
 
@@ -37,3 +37,40 @@ function draw(context, x, y) {
     context.stroke();
     [lastX, lastY] = [x, y];
 }
+
+
+var previousDataUrl = null;
+var downloadTimeout = null;
+
+setInterval(function() {
+    var dataUrl = canvas.toDataURL('image/png');
+
+    // Check if the canvas is blank or hasn't changed
+    var isBlank = checkIfBlank(canvas);
+    if (dataUrl === previousDataUrl || isBlank) {
+        return;
+    }
+
+    // If the canvas has changed, reset the download timeout
+    clearTimeout(downloadTimeout);
+    downloadTimeout = setTimeout(function() {
+        downloadImage(dataUrl);
+    }, 2000);  // 2 seconds
+
+    previousDataUrl = dataUrl;
+}, 1000);
+
+function checkIfBlank(canvas) {
+    var blank = document.createElement('canvas');
+    blank.width = canvas.width;
+    blank.height = canvas.height;
+    return canvas.toDataURL() === blank.toDataURL();
+}
+
+function downloadImage(dataUrl) {
+    var link = document.createElement('a');
+    link.href = dataUrl;
+    link.download = 'canvas.png';
+    link.click();
+}
+
