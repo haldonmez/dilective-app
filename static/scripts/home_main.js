b3519d694d888a5e193e63b2bac5655d0d74e785
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-06-23 00:08:47 +0300

    Update
    Page and gif addition and changes

--- static/scripts/home_main.js
+++ static/scripts/home_main.js
@@ -3,43 +3,68 @@ document.querySelector('.scroll-indicator').addEventListener('click', function(e
     document.querySelector('#second-page').scrollIntoView({ behavior: 'smooth' });
 });
 
+// Function to show the loader
+function showLoader() {
+  document.getElementById('loader-container').style.display = 'flex';
+}
+
+// Function to hide the loader
+function hideLoader() {
+  document.getElementById('loader-container').style.display = 'none';
+}
+
+// Function to navigate to the Learn page
+function navigateToLearn() {
+  document.body.classList.add('fade-out');
+  showLoader();
+  setTimeout(function() {
+      window.location.href = '/learn.html';
+  }, 2000);
+}
+
+
+// Function to navigate to the Listen page
+function navigateToListen() {
+  document.body.classList.add('fade-out');
+  showLoader();
+  setTimeout(function() {
+      window.location.href = '/listen.html';
+  }, 2000);
+}
+
+// Function to navigate to the Index page
+function navigateToIndex() {
+  document.body.classList.add('fade-out');
+  showLoader();
+  setTimeout(function() {
+      window.location.href = '/index.html';
+  }, 2000);
+}
+
+// Functions to show and hide text on hover
 function showText1() {
-  hideAllTexts();
-  document.getElementById('text1').classList.add('vis');
+  document.getElementById('text1').style.opacity = '1';
 }
 
 function hideText1() {
-  document.getElementById('text1').classList.remove('vis');
+  document.getElementById('text1').style.opacity = '0';
 }
 
 function showText2() {
-  hideAllTexts();
-  document.getElementById('text2').classList.add('vis');
+  document.getElementById('text2').style.opacity = '1';
 }
 
 function hideText2() {
-  document.getElementById('text2').classList.remove('vis');
+  document.getElementById('text2').style.opacity = '0';
 }
 
 function showText3() {
-  hideAllTexts();
-  document.getElementById('text3').classList.add('vis');
+  document.getElementById('text3').style.opacity = '1';
 }
 
 function hideText3() {
-  document.getElementById('text3').classList.remove('vis');
+  document.getElementById('text3').style.opacity = '0';
 }
 
-function hideAllTexts() {
-  document.getElementById('text1').classList.remove('vis');
-  document.getElementById('text2').classList.remove('vis');
-  document.getElementById('text3').classList.remove('vis');
-}
 
-function navigateToIndex() {
-  window.location.href = "/index.html";
-}
 
-function navigateToLearn() {
-  window.location.href = "/learn.html";
-}
