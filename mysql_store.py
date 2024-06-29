bd48d615b8552378105212d40eee9c6fe2d95bfd
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-06-30 00:53:31 +0300

    Update
    SQL Fixes

--- mysql_store.py
+++ mysql_store.py
@@ -19,7 +19,6 @@ MyDB = mysql.connector.connect(
 
 MyCursor = MyDB.cursor()
 
-
 # Create the database if it doesn't exist
 create_database(MyCursor)
 
@@ -47,25 +46,3 @@ def RetrieveBlob(ID):
     with open(StoreFilePath, "wb") as File:
         File.write(MyResult)
         File.close()
-
-
-
-def fetch_image_blob(image_id):
-    SQLStatement = "SELECT Photo FROM Images WHERE id = %s"
-    MyCursor.execute(SQLStatement, (image_id,))
-    blob_data = MyCursor.fetchone()[0]
-    return blob_data
-
-def plot_image_from_blob(blob_data):
-    # Convert blob data to PIL Image
-    image = Image.open(BytesIO(blob_data))
-    
-    # Display the image using matplotlib
-    plt.imshow(image, cmap='gray')  # Use grayscale colormap
-    plt.axis('off')  # Hide axes
-    plt.show()
-
-# Example: Fetching and plotting the 10th image (image_id = 18)
-image_id = 22
-blob_data = fetch_image_blob(image_id)
-plot_image_from_blob(blob_data)
