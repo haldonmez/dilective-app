14efb288eae39df49b032e586d4f7690e4694ed5
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-11-11 16:14:05 +0100

    File fixes

---
+++ mysql_store.py
@@ -0,0 +1,48 @@
+import mysql.connector
+import config
+from PIL import Image
+from io import BytesIO
+import matplotlib.pyplot as plt
+
+# Use the variables from config file
+password = config.password
+
+# Function to create the database if it doesn't exist
+def create_database(cursor):
+    cursor.execute("CREATE DATABASE IF NOT EXISTS culs")
+
+MyDB = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = password,
+)
+
+MyCursor = MyDB.cursor()
+
+# Create the database if it doesn't exist
+create_database(MyCursor)
+
+MyDB.database = "culs" # Controlled, User, 
+
+MyCursor.execute("""
+    CREATE TABLE IF NOT EXISTS Images (
+        id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        ClassName VARCHAR(25) NOT NULL,
+        Photo MEDIUMBLOB NOT NULL
+    )
+""")
+
+def InsertBlob(class_name, BinaryData):
+    SQLStatement = "INSERT INTO Images (ClassName, Photo) VALUES (%s, %s)"
+    MyCursor.execute(SQLStatement, (class_name, BinaryData, ))
+    MyDB.commit()
+
+def RetrieveBlob(ID):
+    SQLStatement = "SELECT * FROM Images WHERE id = '{0}'"
+    MyCursor.execute(SQLStatement.format(str(ID)))
+    MyResult = MyCursor.fetchone()[1]
+    StoreFilePath = "ImageOutputs/img{0}.jpeg".format(str(ID))
+    print(MyResult)
+    with open(StoreFilePath, "wb") as File:
+        File.write(MyResult)
+        File.close()
